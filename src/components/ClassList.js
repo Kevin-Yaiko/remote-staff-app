96b14f04477e7f2056b695aeb3d7f3eee794bf7d
Kevin-Yaiko <itd07@yahoo.com>
2023-11-29 19:00:52 -0500

    Adds some json-server stuff for testing.

--- src/components/ClassList.js
+++ src/components/ClassList.js
@@ -1,18 +1,25 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 
 export const ClassList = () => {
   const [classes, setClasses] = useState([])
+  const [url, setUrl] = useState("http://localhost:8000/classes")
+
+  const fetchClasses = useCallback(async() => {
+    const response = await fetch(url)
+    const data = await response.json()
+    setClasses(data)
+  }, [url])
 
   useEffect(() => {
-    fetch("http://localhost:8000/classes")
-    .then(response => response.json())
-    .then(data => setClasses(data))
-  }, [])
+    fetchClasses()
+  }, [fetchClasses])
 
   return (
     <section>
+      <button onClick={() => setUrl("http://localhost:8000/classes")}>All</button>
+      <button onClick={() => setUrl("http://localhost:8000/classes?lessons=16")}>16 lessons</button>
       {classes.map((classItem, index) => (
-        <p key={index}>{classItem.id}</p>
+        <p key={index}>{classItem.name} ({classItem.id})</p>
       ))}
     </section>
   )
